692200fe9ca943b4302ecdc02485f9e8bc81c1ab
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2016-05-13 12:39:30 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 /etc/init.d/cron start
 /etc/init.d/apache2 start
+sudo -u otrs /opt/otrs/bin/otrs.Console.pl Admin::Package::Reinstall OTRSMasterSlave
 tail -f /var/log/apache2/error.log
