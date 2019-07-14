02d8be077e71e039d2216f3b03a1181a35df16df
snoopyathome <mail-an-muehlbauer@web.de>
2019-07-14 12:55:39 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cp /defaults/* /etc/apache2/sites-enabled/
-sed -i "s/EMAIL/${EMAIL}/g" /etc/apache2/sites-enabled/000-default.conf
 /etc/init.d/cron start
 /etc/init.d/apache2 start
 sudo -u otrs /opt/otrs/bin/otrs.Console.pl Admin::Package::Reinstall OTRSMasterSlave
