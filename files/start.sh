aecb3c05060bf31cab49bf4f9bca9fe4c474020b
snoopyathome <mail-an-muehlbauer@web.de>
2019-09-30 21:37:39 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -2,5 +2,4 @@
 cp /defaults/* /etc/apache2/sites-enabled/
 /etc/init.d/cron start
 /etc/init.d/apache2 start
-sudo -u otrs /opt/otrs/bin/otrs.Console.pl Admin::Package::Reinstall OTRSMasterSlave
 tail -f /var/log/apache2/error.log
